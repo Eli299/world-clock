76d2ea30dcc6c7d6910001b2f51e7c1b53231637
Elise Barnard <111289554+Eli299@users.noreply.github.com>
2023-01-24 19:58:41 +0700

    added link to all cities

--- Javascript/index.js
+++ Javascript/index.js
@@ -38,7 +38,7 @@ function updateCity(event) {
   <div class="date">${cityTime.format("MMMM Do YYYY")}</div></div>
   <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
-  )}</small></div></div>`;
+  )}</small></div></div><a href="index.html">All cities</a>`;
 }
 
 updateTime();
